fb14dbcd71a1574857ab5f3a90eb74be883f2ba9
yulangu <108249815+yulangu@users.noreply.github.com>
2024-07-19 17:17:57 -0400

    Delete week2 directory

--- week2/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fontAdjuster = document.getElementById('fontAdjuster');
-    
-    fontAdjuster.addEventListener('change', function() {
-        document.body.style.fontFamily = this.value;
-    });
-});
